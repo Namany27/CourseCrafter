06e6d8232a16fe50674eb1842b8cd27eb007d300
Sheetaly098 <sheetaly098@gmail.com>
2025-04-16 19:54:32 +0530

    Update navbar.tsx

--- ToolsAlchemy/src/app/components/navbar.tsx
+++ ToolsAlchemy/src/app/components/navbar.tsx
@@ -5,6 +5,17 @@ export default function Navbar() {
         <div className="max-w-5xl mx-auto flex justify-between items-center">
           <h1 className="text-lg font-bold">Tools Alchemy</h1>
           <div className="space-x-4 text-sm">
+<a
+  href="https://www.buymeacoffee.com/ToolAlchemy"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <img
+    src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+    alt="Buy Me A Coffee"
+    style={{ height: "45px", width: "162px" }}
+  />
+</a>
             <a href="/" className="hover:underline">
               Home
             </a>
